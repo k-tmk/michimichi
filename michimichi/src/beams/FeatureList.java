0f20f9147d29cdfd833bdfc9ea0b29316b59265b
aohoriuchi <ao.horiuchi@gmail.com>
2022-05-30 15:51:58 +0900

    【堀内】FeatureList.java、M_facilitiesDAO.java、web.xml更新しました。

--- michimichi/src/beams/FeatureList.java
+++ michimichi/src/beams/FeatureList.java
@@ -1,5 +1,46 @@
 package beams;
 
-public class FeatureList {
+import java.io.Serializable;
+
+public class FeatureList implements Serializable {
+	//フィールドのカプセル化
+	private int feature_id;
+	private String feature_name;
+	private String main_photo_path;
+
+	//コンストラクタの定義
+	public FeatureList(){}
+
+	public FeatureList(int feature_id, String feature_name, String main_photo_path){
+
+		this.feature_id = feature_id;
+		this.feature_name = feature_name;
+		this.main_photo_path = main_photo_path;
+
+	}
+
+	public int getId() {
+		return feature_id;
+	}
+
+	public void setId(int feature_id) {
+		this.feature_id = feature_id;
+	}
+
+	public String getName() {
+		return feature_name;
+	}
+
+	public void setName(String feature_name) {
+		this.feature_name = feature_name;
+	}
+
+	public String getPhotoPass() {
+		return main_photo_path;
+	}
+
+	public void setPhotoPass(String main_photo_path) {
+		this.main_photo_path = main_photo_path;
+	}
 
 }
--- michimichi/src/dao/M_facilitiesDAO.java
+++ michimichi/src/dao/M_facilitiesDAO.java
@@ -1,5 +1,75 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import beams.FeatureList;
+import model.DbConnection;
+
 public class M_facilitiesDAO {
+
+
+	/*********
+	 * 特集一覧、出力用のメソッド
+	 *********/
+	public List<FeatureList> findAll(){
+
+		List<FeatureList> FLList = new ArrayList<>();
+
+		String sql = "";
+
+		//データベースに接続
+		Connection conn = null;
+		conn = DbConnection.conn;
+		if(conn == null) return null;
+
+
+		//BDへ接続
+		try{
+
+			//SELECT文を準備
+			//show_flagで表示になっているものを、降順で抽出する。
+			sql = "SELECT * "
+					+ " FROM t_features "
+					+ " WHERE show_flag = 1"
+					+ " ORDER BY"
+					+ " feature_id DESC";
+
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			//SELECTを実行し、結果表を取得
+			ResultSet rs = pStmt.executeQuery();
+
+			//結果表に格納されたレコードの内容をFeatureListインスタンスに設定し、ArrayListインスタンスに追加
+			while (rs.next()){
+				int feature_id = rs.getInt("feature_id");
+				String feature_name = rs.getString("feature_name");
+				String main_photo_path = rs.getString("main_photo_path");
+
+				FeatureList feature = new FeatureList(feature_id,feature_name,main_photo_path);
+				FLList.add(feature);
+			}
+
+	} catch (SQLException e) {
+		// 自動生成された catch ブロック
+		e.printStackTrace();
+		return null;
+
+	}finally {
+		//データベース切断
+
+	}
+	return FLList;
+
+	}
+
+
+	/*********
+	 * 特集個別、出力用のメソッド
+	 *********/
 
 }
